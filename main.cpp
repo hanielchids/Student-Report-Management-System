94c891810af33578a5ea7c819ac52e297ba6eafa
Haniel Chidavose <Haniel.Chidavose@sovtech.com>
2022-08-15 15:57:53 +0200

    added getdata function

--- main.cpp
+++ main.cpp
@@ -32,3 +32,25 @@ grade='C';
 else
 grade='F';
 }
+
+void student::getdata()
+{
+cout<<"\nEnter student's roll number: ";
+cin>>rollno;
+cout<<"\n\nEnter student name: ";
+cin.ignore();
+cin.getline(name,50);
+cout<<"\nAll marks should be out of 100";
+cout<<"\nEnter marks in English: ";
+cin>>eng_marks;
+cout<<"\nEnter marks in Math:  ";
+cin>>math_marks;
+cout<<"\nEnter marks in Science:  ";
+cin>>sci_marks;
+cout<<"\nEnter marks in 2nd language:  ";
+cin>>lang2_marks;
+cout<<"\nEnter marks in Computer science:  ";
+cin>>cs_marks;
+calculate();
+}
+
